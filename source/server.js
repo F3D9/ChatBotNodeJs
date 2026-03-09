ea7088ffc08a8125dd0601b9c664e0bf4658e3e0
Federico Salgado <fsalgado@fi.uba.ar>
2026-03-09 20:23:13 +0000

    optimizo el uso de la api de gemini

--- source/server.js
+++ source/server.js
@@ -24,11 +24,27 @@ const genAI = new GoogleGenerativeAI(process.env.GEMINI_API_KEY);
 async function generateContent() {
     const inputClient = document.getElementById("input").value;
 
-    const model = genAI.getGenerativeModel({ model: "gemini-2.5-flash" });
+    const model = genAI.getGenerativeModel({ 
+        model: "gemini-2.5-flash",
+        systemInstruction: "Sos el asistente de mi aplicación web.Reglas obligatorias:1_ Máximo 3 frases por respuesta.2- Nunca más de 80 palabras.3- Nunca hagas guías largas.4- Nunca uses títulos o secciones.5- Responde claro y corto. "
+     });
 
     const prompt = inputClient;
 
-    const result = await model.generateContent(prompt);
+    //const result = await model.generateContent(prompt);
+    const result = await model.generateContent({
+    contents: [{ role: "user", parts: [{ text: prompt }] }],
+    generationConfig: {
+        maxOutputTokens: 150
+    }});
+
+    let response = result.response.text();
+
+    const words = response.split(" ");
+    if (words.length > 80) {
+    response = words.slice(0, 80).join(" ");
+    }
+
     console.log(result.response.text());
 }
 
